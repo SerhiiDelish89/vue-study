06dd7b8099085bcbd2e4b9e0e4de4b8ecee354ca
Main <main@Karens-MacBook-Air.local>
2023-03-20 13:01:04 +1100

    Added select ( CustomerSelect)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h2>{{ text }}</h2>
+  <CustomSelect :items="['name', 'label', 'salary']"/>
   <CustomInput v-model="text"/>
   <ApartmentsList :items="apartments ">
     
@@ -22,6 +23,7 @@
 import ApartmentsList from "./components/apartment/ApartmentsList.vue";
 import ApartmentsItem from "./components/apartment/ApartmentsItem.vue";
 import CustomInput from './components/shared/CustomInput.vue'
+import CustomSelect from './components/shared/CustomSelect.vue'
 import apartments from "./components/apartment/apartments";
 
 export default {
@@ -30,6 +32,7 @@ export default {
     ApartmentsList,
      ApartmentsItem,
      CustomInput,
+     CustomSelect,
   },
   data() {
     return {
